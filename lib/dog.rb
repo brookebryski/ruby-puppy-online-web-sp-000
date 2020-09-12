372e12dd9e36258a3c1bd91cc2680d727866d428
chic-state-0895 <brookeabryski@gmail.com>
2020-09-12 18:21:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Add your code here
+class Dog
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def all
+    @@all
+  end
+
+  def print_all(array)
+    array.each do |name|
+      puts "#{name}"
+    end
+  end
+end
+
